b077b52dcb72496e0fe8bf40257e76a55b8493c1
Karan <karanchaudhary7326@gmail.com>
2023-08-05 00:09:57 +0530

    Initial commit

---
+++ Hospital_Managment/HospitalManagement_Django/hospital/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Doctor,Patient,Appointment,PatientDischargeDetails
+class DoctorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Doctor, DoctorAdmin)
+
+class PatientAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Patient, PatientAdmin)
+
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Appointment, AppointmentAdmin)
+
+class PatientDischargeDetailsAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(PatientDischargeDetails, PatientDischargeDetailsAdmin)
